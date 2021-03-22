e674aa54085282d32596ccb9eb232e91f4cce4ae
U-K-L <WilliamsNoah1720@gmail.com>
2021-03-22 12:09:44 -0400

    Added XR RIG

---
+++ Assets/Scripts/C2M2/ControllerGuide/ControllerLineRender.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ControllerLineRender : MonoBehaviour
+{
+    [SerializeField] public Transform[] points;
+    [SerializeField] public ControllerGuideLineRender line;
+
+    private void Start()
+    {
+        line.SetUpLine(points);
+    }
+}
